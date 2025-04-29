9345dbc4bf1377ced9c1240f3794891108e6cebe
申公 <shengong.yz@alibaba-inc.com>
2025-04-29 10:57:29 +0800

    feat: init oio main class

---
+++ practice/src/main/java/com/alibaba/aidc/nio/OioSocketHandler.java
@@ -0,0 +1,15 @@
+package com.alibaba.aidc.nio;
+
+public class OioSocketHandler {
+
+
+    public static void main(String[] args) {
+
+        while(Thread.interrupted()) {
+            System.out.println("thread interrupted!");
+        }
+
+
+    }
+
+}
